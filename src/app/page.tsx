cd3d275bd5c08b083f4a08a46abe913eac0f285a
William Avila <wyllyamaxl@hotmail.com>
2024-10-23 14:55:48 -0300

    editando site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+'use client'
+import { Header } from "@/components/Header";
+import CardPromocao from "@/components/CardPromocao";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+
+
+  return (
+     <main>
+
+        <div className="flex flex-col items-center mt-20 mx-auto">
+            <div className="max-w-xl p-6 bg-white  border-gray-200  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+                <h5 className="flex items-center text-center mb-2 text-7xl font-bold text-blue-500 dark:text-white">Gestao Studio</h5>
+               <p className="flex text-center text-3xl font-normal mt-10 text-gray-500 dark:text-gray-400">Sua plataforma completa para gerenciar, agendar e comandar seu salão.</p>
+               <div className="flex flex-row align-center mt-2 px-10 py-10 mx-auto">
+                  <button type="button" className="px-6 py-3.5 text-base mx-10 font-medium text-center  text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Quero começar</button>
+                  <button type="button" className="px-6 py-3.5 text-base font-medium text-center  text-white bg-blue-950 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Já sou cliente</button>
+               </div>
+            </div>
+        
+            <div className="flex flex-row items-center mt-20 mb-20 px-10 py-10 mx-auto">
+               <div className="max-w-sm p-6 bg-white border-gray-200  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+                  <h5 className="text-center mb-2 text-4xl font-bold text-blue-500 dark:text-white">Rápido</h5>
+                  <p className="m-10 font-normal text-gray-500 dark:text-gray-400">Do cadastro ao primeiro cliente em segundos!</p>
+               </div>
+
+               <div className="max-w-sm p-6 bg-white border-gray-200  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+                  <h5 className="text-center mb-2 text-4xl font-bold  text-blue-500 dark:text-white">Fácil</h5>
+                  <p className="m-10 font-normal text-gray-500 dark:text-gray-400">Tudo pelo celular, sem se procupado com agenda de papel</p>
+               </div>
+               <div className="max-w-sm p-6 bg-white border-gray-200  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+                  <h5 className="text-center mb-2 text-4xl font-bold  text-blue-500 dark:text-white">Seguro</h5>
+                  <p className="m-10 font-normal text-gray-500 dark:text-gray-400">Seus clientes sao seus. Nós nunca venderemos seus dados</p>
+               </div>
+            </div>
+
+            <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-20">
+  
+              <CardPromocao />
+              <CardPromocao />
+              <CardPromocao />
+              
+            </div>
+
+            <div>
+
+            </div>
+
+            <div className="max-w-xl p-6 mb-20 mt-10 bg-white  border-gray-200  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+              <h5 className="flex items-center mb-2 text-5xl font-bold tracking-tight text-blue-950 dark:text-white">Comece hoje mesmo</h5>
+              <div className="flex flex-col align-center mt-2 px-10 py-10 mx-auto">
+                <button type="button" className="px-6 py-3.5 text-base mx-10 font-medium text-center  text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Quero começar</button>
+              </div>
+            </div>
+
+
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+     </main>
   );
 }
---
+++ src/components/CardPromocao.tsx
@@ -0,0 +1,32 @@
+export default function CardPromocao () {
+    return (
+
+        
+
+        
+        <div className="flex flex-row items-center">
+            <div className="max-w-sm m-3 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img className="rounded-br-3xl rounded-bl-lg rounded-tl-3xl rounded-tr-lg" src="barbearia.jpg" alt="" />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-blue-950 dark:text-white">Barbearia</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Você vai liberar tempo na rotina da sua barbearia para vender mais com um sistema de gestão completo.</p>
+                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">* Controle de agenda online.</p>
+                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">* App para profissionais.</p>
+                    <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">* Clube de Assinaturas.</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Soluções para Barbearia
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
