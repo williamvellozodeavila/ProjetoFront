96caa779d292c72aef2dcff56731e3d29a741bb9
William Avila <wyllyamaxl@hotmail.com>
2024-10-29 17:33:13 -0300

    arrumando cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -41,29 +41,29 @@ export default function Cadastro() {
 
     <section className="bg-white dark:bg-gray-900">
       <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-gray-200 rounded-lg shadow dark:border md:mt-20 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-1/4 bg-gray-100 border-gray-300 border-2 rounded-lg shadow-lg dark:border m-20 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(verificaCadastro)}>
               <div>
                 <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome Completo:</label>
-                <input type="text" id="nome" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                <input type="text" id="nome" className="bg-gray-50 border-2 border-gray-300  text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Elma Maria"
                   required 
                   {...register("nome")} />
               </div>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail:</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                <input type="email" id="email" className="bg-gray-50 border-2 border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nome@exemplo.com"
                   required 
                   {...register("email")} />
               </div>
               <div>
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Senha:</label>
-                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                <input type="password" id="password" className="bg-gray-50 border-2 border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••"
                   required 
                   {...register("senha")} />
               </div>
-              <button type="submit" className="w-full text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+              <button type="submit" className="w-full text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium shadow-lg rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Criar Conta
               </button>
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -60,14 +60,14 @@ export default function Login() {
         personalizado do seu jeito</h2>
       </div>
       
-      <div className="flex-col items-center w-1/3 ml-10">
-        <div className="w-full bg-gray-100 rounded-lg shadow dark:border md:mt-20 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex-col items-center w-1/4 ml-10 shadow-lg">
+        <div className="w-full bg-gray-100 rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <form className="space-y-4 md:space-y-6" 
               onSubmit={handleSubmit(verificaLogin)}>
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail:</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nome@email.com" 
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nome@exemplo.com" 
                   required 
                   {...register("email")} />
               </div>
@@ -92,10 +92,10 @@ export default function Login() {
                 
               </div>
               <div className="flex flex-row">             
-                <button type="submit" className="w-full text-white bg-blue-400 hover:bg-primary-700 focus:ring-4 focus:outline-none m-1 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                <button type="submit" className="blue-button">
                   Entrar
                 </button>
-                <a href="/alterar-senha" className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none m-1 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                <a href="/alterar-senha" className="dark-button">
                   Esqueci a senha
                 </a>
               </div>
